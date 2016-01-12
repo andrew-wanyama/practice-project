b7afa4c5d0c95805cf5430901a19166e49b655ce
Andrew Wanyama <andywanyama@gmail.com>
2016-01-12 09:42:12 +0300

    Made test to pass

--- test/SampleClassTest.php
+++ test/SampleClassTest.php
@@ -3,6 +3,6 @@ class SampleClassTest extends PHPUnit_Framework_TestCase {
     //test SampleClass is instantiated
     public function testFailure(){
         $actual = new \Test\SampleClass();
-        $this->assertInstanceOf('\Test\SampleClas', $actual);            
+        $this->assertInstanceOf('\Test\SampleClass', $actual);            
     }
 }
