30df32cb5ad755fbbf0a2dbda02729ff1faa7182
Andrew Wanyama <andywanyama@gmail.com>
2016-01-24 16:59:08 +0300

    Assert the right string representation of array is returned

--- src/SMSApplication/CLIArrayTable.php
+++ src/SMSApplication/CLIArrayTable.php
@@ -1,36 +1,47 @@
 <?php
-namespace Test;
+
+namespace SMSApplication;
 
 class CLIArrayTable {
-    private $myArray;//stores the 2D array
+
+    private $myArray; //stores the 2D array
     private $array_columns = [];
+
     //constructor ensures we pass a 2D array once the object is created
-    public function __construct($myArray) {
-        $this->myArray = $myArray;
+    public function __construct(array $my2DArray) {
+        foreach ($my2DArray as $myArray) {
+            if (!is_array($myArray)) {
+                die("Sorry, constructor requires a 2D array argument.\n");
+            } else {
+                $this->myArray = $my2DArray;
+            }
+        }
     }
-    
-    public function showArrayTable(){
-        foreach ($this->myArray as $myArrays){
+
+    public function getArray() {
+        return $this->myArray;
+    }
+
+    public function toString() {
+        foreach ($this->myArray as $myArrays) {
             //loop through inner arrays
-            foreach ($myArrays as $key => $value){
+            foreach ($myArrays as $key => $value) {
                 //array_column returns values from multi dimensional array based on column keys
                 $this->array_columns[$key] = array_column($this->myArray, $key);
             }
         }
         //loop through array-columns and format output
-        foreach ($this->array_columns as $column_key => $column_value){
-            $output = print "\n{$column_key}\n........\n";
-            foreach ($column_value as $index => $val){
-                $output .= print "{$index}|{$val}\n";
+        foreach ($this->array_columns as $column_key => $column_value) {
+            $output = "\n{$column_key}\n........\n";
+            foreach ($column_value as $index => $val) {
+                $output .= "{$index}|{$val}\n";
             }
         }
         return $output;
     }
-    
-    public function __toString(){
-        return $this->showArrayTable();
-    }
-}
 
-$arrayTable = new CLIArrayTable([['foo'=>'bar','foo2'=>'bar2','foo3'=>'bar3'],['foo'=>'bar','foo2'=>'baz2']]);
-echo $arrayTable;
+    public function __toString() {
+        return $this->toString();
+    }
+
+}
--- test/CLIArrayTableTest.php
+++ test/CLIArrayTableTest.php
@@ -1,20 +1,25 @@
 <?php
-class CLIArrayTableTest extends PHPUnit_Framework_TestCase{
+
+class CLIArrayTableTest extends PHPUnit_Framework_TestCase {
+
     private $arrayTable;
+
     //instantiate an object of the class
     public function setUp() {
-        $this->arrayTable = new \Test\CLIArrayTable([['foo'=>'bar'],['foo'=>'baz']]);
+        $this->arrayTable = new \SMSApplication\CLIArrayTable([['foo' => 'bar'], ['foo' => 'baz']]);
     }
-    //test for correct values within the array
-    /*
-    public function testShowArrayTable(){
-        $expected = $this->arrayTable->showArrayTable();
-        $this->assertEquals($expected, 'bar');
-    }
-     * 
-     */
+
     //test that output is a string
-    public function testShowArrayTable() {
-        $this->assertTrue(is_string($this->arrayTable->showArrayTable()));
+    public function testToString() {
+        $this->assertTrue(is_string($this->arrayTable->toString()));
     }
+
+    //assert correct representation of array as a string
+    public function testToString_RightFormat() {
+        $strOutput = "\nfoo\n........\n";
+        $strOutput .= "0|bar\n";
+        $strOutput .= "1|baz\n";
+        $this->assertEquals($this->arrayTable->toString(), $strOutput);
+    }
+
 }
