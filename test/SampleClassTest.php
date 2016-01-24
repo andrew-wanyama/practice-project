501c31b6da635e344f650dc65f79255f5eee6024
Andrew Wanyama <andywanyama@gmail.com>
2016-01-24 13:54:06 +0300

    remove SampleClass files

--- test/SampleClassTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-class SampleClassTest extends PHPUnit_Framework_TestCase {
-    //test SampleClass is instantiated
-    public function testFailure(){
-        $actual = new \Test\SampleClass();
-        $this->assertInstanceOf('\Test\SampleClass', $actual);            
-    }
-}
